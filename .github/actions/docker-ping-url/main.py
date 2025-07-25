798312165a396c8396f69f35fd41295674eccd2e
Christian Hettlage <hettlage@saao.ac.za>
2025-07-25 20:48:07 +0200

    Implement the pinging functionality

--- .github/actions/docker-ping-url/main.py
+++ .github/actions/docker-ping-url/main.py
@@ -1,2 +1,26 @@
+import os
+from time import sleep
+
+import requests
+
+
+def ping_url(url, delay, max_trials) -> bool:
+    trial = 1
+    while trial <= max_trials:
+        response = requests.get(url)
+        if response.ok:
+            return True
+        sleep(delay)
+    return False
+
+
+def run():
+    url = os.environ["INPUT_URL"]
+    max_trials = int(os.environ.get("INPUT_MAX_TRIALS"), 10)
+    delay = int(os.environ.get("INPUT_DELAY"), 5)
+    ping_successful = ping_url(url, max_trials, delay)
+    if not ping_successful:
+        raise Exception(f"The url {url} could not be pinged successfully.")
+
 if __name__ == "__main__":
-    print("Hello world")
+    run()
