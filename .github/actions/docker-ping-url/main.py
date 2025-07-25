4cb07064af24e7496a8ba76d0a0614db80eead68
Christian Hettlage <hettlage@saao.ac.za>
2025-07-25 20:51:00 +0200

    Fix the Python code

--- .github/actions/docker-ping-url/main.py
+++ .github/actions/docker-ping-url/main.py
@@ -16,8 +16,8 @@ def ping_url(url, delay, max_trials) -> bool:
 
 def run():
     url = os.environ["INPUT_URL"]
-    max_trials = int(os.environ.get("INPUT_MAX_TRIALS"), 10)
-    delay = int(os.environ.get("INPUT_DELAY"), 5)
+    max_trials = int(os.environ.get("INPUT_MAX-TRIALS", 10))
+    delay = int(os.environ.get("INPUT_DELAY", 5))
     ping_successful = ping_url(url, max_trials, delay)
     if not ping_successful:
         raise Exception(f"The url {url} could not be pinged successfully.")
